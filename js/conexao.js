bacca3ffa47cecb5a52bc1917ceda40997a9e81d
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2023-03-10 07:27:42 -0300

    update

---
+++ js/conexao.js
@@ -0,0 +1,80 @@
+let enderecoCarteira;
+let saldoCarteira;
+
+// Quando o botão "Conectar" for clicado
+document.getElementById('conectar').addEventListener('click', async () => {
+    // Verificar se a carteira Metamask está instalada
+    if (typeof window.ethereum !== 'undefined') {
+      try {
+        // Solicitar acesso à carteira Metamask
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        // Conectar à carteira Metamask
+        const web3 = new Web3(window.ethereum);
+        const accounts = await web3.eth.getAccounts();
+        const balanceWei = await web3.eth.getBalance(accounts[0]);
+
+        const Metamask = true;
+        if (Metamask == true) {
+          Swal.fire({
+            title: 'Carteira conectada!',
+            text: "Login realizado com sucesso!",
+            icon: 'success',
+            confirmButtonColor: '#00C171',
+          }).then((result) => {
+            // Exibir o endereço da carteira conectada
+            
+            enderecoCarteira = accounts[0];
+            const enderecoElemento = document.getElementById('endereco');
+            enderecoElemento.textContent = `Endereço : \n\n ${accounts[0]}`;
+            const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
+            saldoCarteira = balanceEth;
+            const saldoElemento = document.getElementById('saldo');
+            saldoElemento.textContent = `Saldo :  ${balanceEth} ETH`;
+            const info = document.getElementById('info');
+            info.textContent = `Informações da carteira:`;
+            const continuar = document.getElementById('continuar');
+            continuar.textContent = `Continuar`;
+            continuar.style.background = "#00C171";
+          })
+        }
+
+
+      } catch (error) {
+        // O usuário não concedeu permissão ou algo deu errado
+        console.error(error);
+      }
+    } else {
+      // A carteira Metamask não está instalada
+      alert('Por favor, instale a carteira Metamask para se conectar.');
+    }
+  });
+
+  function createHash() {
+    const inputValue = document.getElementById('inputValue').value;
+    const hash = CryptoJS.SHA256(inputValue);
+    const hashImeiResult = document.getElementById('hash');
+    hashImeiResult.textContent = `${hash}`;
+    const hashAparelho = document.getElementById('hashAparelho');
+    hashAparelho.textContent = `IMEI do aparelho : `;
+    hashImeiResult.style.background = "#5f5f5f";
+  }
+
+  document.getElementById('continuar').addEventListener('click', async () => {
+    window.location.href = "../html/home.html";
+  });
+  
+//   // Quando o botão "Desconectar" for clicado
+// document.getElementById('desconectar').addEventListener('click', async () => {
+//     try {
+//       // Desconectar da carteira Metamask
+//       alert('Desconectado com sucesso!');
+//       // Limpar os elementos HTML que exibem os dados da carteira
+//       const enderecoElemento = document.getElementById('endereco');
+//       enderecoElemento.textContent = '';
+//       const saldoElemento = document.getElementById('saldo');
+//       saldoElemento.textContent = '';
+//     } catch (error) {
+//       // O usuário não concedeu permissão ou algo deu errado
+//       console.error(error);
+//     }
+// });
